d5196f5183e49a4c37e1b6229c6ce4b69a299be6
iasmoke <makssimgerasimov@gmail.com>
2020-06-05 21:27:44 +0300

    05.06.20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -18,8 +24,14 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
-    
-    
+    MatDialogModule,
+    MatToolbarModule,
+    MatIconModule,
+    NgbModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,37 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../service/login.service';
 import { HttpClient } from '@angular/common/http';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { MustMatch } from './must-match.validator';
 declare var $: any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  user_id = this.loginService.user_id;
-
+  error: any;
   userName = "";
   password = "";
   user_name_title: any;
-  error: any;
+  registerForm: FormGroup;
+  submitted = false;
 
-  pageOne = this.user_id !== null ? true : false;
-  userPage = false;
-  backTestReport = false;
-  login = this.user_id === null ? true : false
+  error_register: any;
 
-  
-  
+  hide_register
 
- 
   constructor(
     private loginService: LoginService,
     private http: HttpClient,
-  ) { }
+    private formBuilder: FormBuilder,
+  ) {
+
+  }
 
   logIn() {
     localStorage.removeItem('user_id')
@@ -47,7 +45,47 @@ export class LoginComponent implements OnInit {
       }
     });
   }
+  register() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return console.log('false');
+    } else {
+      this.loginService.registerUser(this.registerForm.value).subscribe(res => {
+        this.error_register = JSON.parse(res).toString()
+        console.log(this.error_register);
+        if (this.error_register === 'Registration successful') {
+          $("#register_user").modal("hide")
+          $("#myModalError_register").modal('show');
+          setTimeout(function () {
+            $("#myModalError_register").modal('hide');
+          }, 2000);
+        }
+      });
+    }
+
+  }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+      position: ['', Validators.required],
+      user_role: ['', Validators.required]
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
+    });
+  }
+
+  get f() { return this.registerForm.controls; }
 
 
-  ngOnInit() {}
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+  }
 }
