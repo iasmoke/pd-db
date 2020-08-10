ff196264c96df32005baf09173a25cd368cec393
Maks <makssimgerasimov@gmail.com>
2020-08-10 15:43:34 +0300

    окей

--- src/assets/php/user_hr_main_table_get.php
+++ src/assets/php/user_hr_main_table_get.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(error_reporting() & ~E_NOTICE);
-ini_set('memory_limit', '1');
+ini_set('memory_limit', '-1');
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type");
@@ -12,7 +12,7 @@ $_POST = json_decode(file_get_contents('php://input'), true);
 $user_name_create_employee = $_POST['user_name_create_employee'];
 
 
-$sql = "SELECT id_personal, first_name,last_name,second_name,position,type_department,department,number_phone,attraction_channel,attraction_channel_description,interview_date,internship_date,internship_place,certification_date,rejection_reason,`status`,employee_description FROM db_main ";
+$sql = "SELECT id_personal, first_name,last_name,second_name,position,type_department,department,number_phone,attraction_channel,attraction_channel_description,interview_date,internship_date,internship_place,certification_date,rejection_reason,`status`,employee_description FROM db_main";
 if ($stmt = $db_connect->prepare($sql)) {   
     $stmt->execute();
     $stmt->bind_result(
