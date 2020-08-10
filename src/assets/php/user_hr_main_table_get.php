09989a2e962fe7a7070d9070c0d0a93648aae124
Maks <makssimgerasimov@gmail.com>
2020-08-10 11:17:54 +0300

    new scr

--- src/assets/php/user_hr_main_table_get.php
+++ src/assets/php/user_hr_main_table_get.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(error_reporting() & ~E_NOTICE);
-ini_set('memory_limit', '-1');
+ini_set('memory_limit', '1');
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type");
@@ -13,7 +13,7 @@ $user_name_create_employee = $_POST['user_name_create_employee'];
 
 
 $sql = "SELECT id_personal, first_name,last_name,second_name,position,type_department,department,number_phone,attraction_channel,attraction_channel_description,interview_date,internship_date,internship_place,certification_date,rejection_reason,`status`,employee_description FROM db_main WHERE user_name_create_employee=? ORDER BY db_main.id_personal DESC";
-if ($stmt = $db_connect->prepare($sql)) {
+if ($stmt = $db_connect->prepare($sql)) {   
     $stmt->bind_param("s", $user_name_create_employee);
     $stmt->execute();
     $stmt->bind_result(
